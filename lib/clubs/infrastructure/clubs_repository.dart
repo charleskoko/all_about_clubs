74a10c53ced32d7ee250db7b546f1ed8be493ed0
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2022-02-12 17:16:29 +0100

    master: update_clubs_repository.dart

--- lib/clubs/infrastructure/clubs_repository.dart
+++ lib/clubs/infrastructure/clubs_repository.dart
@@ -2,6 +2,7 @@ import 'package:all_about_clubs/clubs/domain/club.dart';
 import 'package:all_about_clubs/clubs/domain/club_failure.dart';
 import 'package:all_about_clubs/core/domain/fresh.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 abstract class ClubsRepository {
   Future<Either<ClubFailure, Fresh<List<Club>>>> getClubs(String filter);
